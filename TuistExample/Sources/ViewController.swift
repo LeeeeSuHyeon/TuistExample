fbec468a14d40e12554d9bab5c4034e49003e565
LeeSuHyeon <tngus0673@naver.com>
2025-08-06 15:57:52 +0900

    커스텀 컬러 / 폰트 추가

--- TuistExample/Sources/ViewController.swift
+++ TuistExample/Sources/ViewController.swift
@@ -11,19 +11,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = TuistExampleAsset.accentColor.color
 
         let label = UILabel()
         label.text = "Hello from UIKit!"
-        label.textColor = .white
+        label.textColor = UIColor(asset: TuistExampleAsset.accentColor)
         label.font = UIFont.boldSystemFont(ofSize: 24)
         label.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(label)
+
+        let imageView = UIImageView()
+        imageView.image = TuistExampleAsset.aImage.image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+//        view.addSubview(label)
+        view.addSubview(imageView)
 
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 450)
+
+//            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+//            imageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+//            imageView.rightAnchor.constraint(equalTo: view.rightAnchor),
+//            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
 }
